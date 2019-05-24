9a8d7507f1983e5ee4ac281d3a1d7f2abd524825
Matthias DOLA <koami.dola@pythagoras-afrique.com>
2019-05-24 12:57:41 +0000

    Improve the code according the reviewer recommendation

--- app/src/main/java/com/example/dola/educationquizapp/MainActivity.java
+++ app/src/main/java/com/example/dola/educationquizapp/MainActivity.java
@@ -8,12 +8,12 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.w3c.dom.Text;
-
 public class MainActivity extends AppCompatActivity {
 
-    private int totalQuiz = 13;
-    private int currentQiuz = 0;
+    int totalQuiz = 15;
+    int currentQiuz = 0;
+    float score = 0;
+    boolean responseGiven = false;
 
     private TextView CurrentQuizTextView, TotalQuizTextView ;
     private EditText nameEdiText, surnameEditText;
@@ -44,31 +44,24 @@ public class MainActivity extends AppCompatActivity {
      * Update the counter of the actual quiz
      */
     void updateCurrentQuizStatus(){
-        TotalQuizTextView.setText("" + totalQuiz);
-        CurrentQuizTextView.setText("" + ++currentQiuz);
+        TotalQuizTextView.setText(String.format("%s", totalQuiz));
+        CurrentQuizTextView.setText(String.format("%s", ++currentQiuz));
     }
 
 
     /**
      *  This method verify that tha user give it firstname and lastname
      *  And then start the quiz page to allow the user to start.
-     * @param view
      */
     public void startQuiz(View view)
     {
-        name = nameEdiText.getText().toString();
-        surname = surnameEditText.getText().toString();
-        if (name.isEmpty() || surname.isEmpty()){
-            Toast.makeText(this, "Please give us your firstname and lastname", Toast.LENGTH_LONG).show();
-            return;
-        }
 
         LinearLayout welcome_container = findViewById(R.id.welcome_container);
         welcome_container.setVisibility(View.GONE);
         LinearLayout startbtn_container = findViewById(R.id.quizapp_start_screen);
         startbtn_container.setVisibility(View.GONE);
-
-        displayNextQuiz();
+        responseGiven = true;
+        displayNextQuiz(view);
         LinearLayout statusContainer = findViewById(R.id.status_container);
         statusContainer.setVisibility(View.VISIBLE);
 
@@ -79,8 +72,13 @@ public class MainActivity extends AppCompatActivity {
     /**
      *  This method display the next quiz
      */
-    void displayNextQuiz(){
+    void displayNextQuiz(View view){
         LinearLayout quizContainer;
+
+         if (!responseGiven){
+            Toast.makeText(this, R.string.give_anwser_msg, Toast.LENGTH_SHORT).show();
+            return;
+        }
 
         hideAllQuizContainer();
         switch (currentQiuz){
@@ -144,9 +142,21 @@ public class MainActivity extends AppCompatActivity {
                 quizContainer = findViewById(R.id.question_13);
                 quizContainer.setVisibility(View.VISIBLE);
                 break;
+
+            case 13:
+                quizContainer = findViewById(R.id.question_14);
+                quizContainer.setVisibility(View.VISIBLE);
+                break;
+
+            case 14:
+                quizContainer = findViewById(R.id.question_15);
+                quizContainer.setVisibility(View.VISIBLE);
+                break;
             default:
                 break;
         }
+
+        responseGiven = false;
         updateCurrentQuizStatus();
     }
 
@@ -171,6 +181,8 @@ public class MainActivity extends AppCompatActivity {
         LinearLayout quiz11Container = findViewById(R.id.question_11);
         LinearLayout quiz12Container = findViewById(R.id.question_12);
         LinearLayout quiz13Container = findViewById(R.id.question_13);
+        LinearLayout quiz14Container = findViewById(R.id.question_14);
+        LinearLayout quiz15Container = findViewById(R.id.question_15);
 
 
         quiz1Container.setVisibility(View.GONE);
@@ -186,6 +198,8 @@ public class MainActivity extends AppCompatActivity {
         quiz11Container.setVisibility(View.GONE);
         quiz12Container.setVisibility(View.GONE);
         quiz13Container.setVisibility(View.GONE);
+        quiz14Container.setVisibility(View.GONE);
+        quiz15Container.setVisibility(View.GONE);
     }
 
     /**
@@ -208,7 +222,7 @@ public class MainActivity extends AppCompatActivity {
         thankContainer.setVisibility(View.VISIBLE);
 
         TextView username = findViewById(R.id.username_in_thank);
-        username.setText(name + " " + surname);
+        username.setText(String.format("%s %s", name, surname));
 
         currentQiuz = 0;
     }
@@ -219,264 +233,303 @@ public class MainActivity extends AppCompatActivity {
 
     public void quest1Answer(View view)
     {
-        if (view.getId() == R.id.quest1_answer_a_container){
-            responses[0] = getResources().getString(R.string.quest_1_a);
+        responseGiven = true;
+        if (view.getId() == R.id.checkbox_quest_1_a){
+            score++;
 
-        } else if (view.getId() == R.id.quest1_answer_b_container){
-            responses[0] = getResources().getString(R.string.quest_1_b);
+        } else if (view.getId() == R.id.checkbox_quest_1_b){
+            score += 0.25;
 
-        }else if (view.getId() == R.id.quest1_answer_c_container){
-            responses[0] = getResources().getString(R.string.quest_1_c);
+        }else if (view.getId() == R.id.checkbox_quest_1_c){
+            score += 0.5;
 
-        }else if (view.getId() == R.id.quest1_answer_d_container){
-            responses[0] = getResources().getString(R.string.quest_1_d);
+        }else if (view.getId() == R.id.checkbox_quest_1_d){
+            score += 0.2;
         }
-        displayNextQuiz();
-
     }
 
     public void quest2Answer(View view)
     {
-        if (view.getId() == R.id.quest2_answer_a_container){
-            responses[1] = getResources().getString(R.string.quest_2_a);
+        responseGiven = true;
+        if (view.getId() == R.id.checkbox_quest_2_a){
+            score += 1;
 
-        } else if (view.getId() == R.id.quest2_answer_b_container){
-            responses[1] = getResources().getString(R.string.quest_2_b);
+        } else if (view.getId() == R.id.checkbox_quest_2_b){
+            score += 0;
 
-        }else if (view.getId() == R.id.quest2_answer_c_container){
-            responses[1] = getResources().getString(R.string.quest_2_c);
+        } else if (view.getId() == R.id.checkbox_quest_2_c){
+            score += 0.25;
 
-        }else if (view.getId() == R.id.quest2_answer_d_container){
-            responses[1] = getResources().getString(R.string.quest_2_d);
+        } else if (view.getId() == R.id.checkbox_quest_2_d){
+            score += 0.1;
 
         }
-        displayNextQuiz();
-
     }
 
     public void quest3Answer(View view)
     {
-        if (view.getId() == R.id.quest3_answer_a_container){
-            responses[2] = getResources().getString(R.string.quest_3_a);
+        responseGiven = true;
+        if (view.getId() == R.id.checkbox_quest_3_a){
+            score += 0.25;
 
-        } else if (view.getId() == R.id.quest3_answer_b_container){
-            responses[2] = getResources().getString(R.string.quest_3_b);
+        } else if (view.getId() == R.id.checkbox_quest_3_b){
+            score += 0.25;
 
-        }else if (view.getId() == R.id.quest3_answer_c_container){
-            responses[2] = getResources().getString(R.string.quest_3_c);
+        } else if (view.getId() == R.id.checkbox_quest_3_c){
+            score += 0.25;
 
-        }else if (view.getId() == R.id.quest3_answer_d_container){
-            responses[2] = getResources().getString(R.string.quest_3_d);
+        } else if (view.getId() == R.id.checkbox_quest_3_d){
+            score += 0.25;
         }
-        displayNextQuiz();
+
     }
 
 
     public void quest4Answer(View view)
     {
-        if (view.getId() == R.id.quest4_answer_a_container){
-            responses[3] = getResources().getString(R.string.quest_4_a);
+        responseGiven = true;
+        if (view.getId() == R.id.checkbox_quest_4_a){
+            score += 0.25;
 
-        } else if (view.getId() == R.id.quest4_answer_b_container){
-            responses[3] = getResources().getString(R.string.quest_4_b);
+        } else if (view.getId() == R.id.checkbox_quest_4_b){
+            score += 0.25;
 
-        }else if (view.getId() == R.id.quest4_answer_c_container){
-            responses[3] = getResources().getString(R.string.quest_4_c);
+        } else if (view.getId() == R.id.checkbox_quest_4_c){
+            score += 0.25;
 
-        }else if (view.getId() == R.id.quest4_answer_d_container){
-            responses[3] = getResources().getString(R.string.quest_4_d);
+        } else if (view.getId() == R.id.checkbox_quest_4_d){
+            score += 0.25;
         }
-        displayNextQuiz();
-
     }
 
 
     public void quest5Answer(View view)
     {
-        if (view.getId() == R.id.quest5_answer_a_container){
-            responses[4] = getResources().getString(R.string.quest_5_a);
-        } else if (view.getId() == R.id.quest5_answer_b_container){
-            responses[4] = getResources().getString(R.string.quest_5_b);
+        responseGiven = true;
+        if (view.getId() == R.id.checkbox_quest_5_a){
+            score += 0.25;
 
-        }else if (view.getId() == R.id.quest5_answer_c_container){
-            responses[4] = getResources().getString(R.string.quest_5_c);
+        } else if (view.getId() == R.id.checkbox_quest_5_b){
+            score += 0.25;
 
-        }else if (view.getId() == R.id.quest5_answer_d_container){
-            responses[4] = getResources().getString(R.string.quest_5_d);
+        }else if (view.getId() == R.id.checkbox_quest_5_c){
+            score += 0.25;
 
-        }else if (view.getId() == R.id.quest5_answer_e_container){
-            responses[4] = getResources().getString(R.string.quest_5_e);
+        }else if (view.getId() == R.id.checkbox_quest_5_d){
+            score += 0.25;
+
+        }else if (view.getId() == R.id.checkbox_quest_5_e){
+            score += 0.25;
 
         }
-        displayNextQuiz();
 
     }
 
 
     public void quest6Answer(View view)
     {
-        if (view.getId() == R.id.quest6_answer_a_container){
-            responses[5] = getResources().getString(R.string.quest_6_a);
+        responseGiven = true;
+        if (view.getId() == R.id.checkbox_quest_6_a){
+            score += 0.25;
 
-        } else if (view.getId() == R.id.quest6_answer_b_container){
-            responses[5] = getResources().getString(R.string.quest_6_b);
+        } else if (view.getId() == R.id.checkbox_quest_6_b){
+            score += 0.25;
 
-        }else if (view.getId() == R.id.quest6_answer_c_container){
-            responses[5] = getResources().getString(R.string.quest_6_c);
+        }else if (view.getId() == R.id.checkbox_quest_6_c){
+            score += 0.25;
 
-        }else if (view.getId() == R.id.quest6_answer_d_container){
-            responses[5] = getResources().getString(R.string.quest_6_d);
+        }else if (view.getId() == R.id.checkbox_quest_6_d){
+            score += 0.25;
 
         }
-        displayNextQuiz();
 
     }
 
 
     public void quest7Answer(View view)
     {
-        if (view.getId() == R.id.quest7_answer_a_container){
-            responses[6] = getResources().getString(R.string.quest_7_a);
+        responseGiven = true;
+        if (view.getId() == R.id.checkbox_quest_7_a){
+            score += 0.25;
 
-        } else if (view.getId() == R.id.quest7_answer_b_container){
-            responses[6] = getResources().getString(R.string.quest_7_b);
+        } else if (view.getId() == R.id.checkbox_quest_7_b){
+            score += 0.25;
 
-        }else if (view.getId() == R.id.quest7_answer_c_container){
-            responses[6] = getResources().getString(R.string.quest_7_c);
+        }else if (view.getId() == R.id.checkbox_quest_7_c){
+            score += 0.25;
 
-        }else if (view.getId() == R.id.quest7_answer_d_container){
-            responses[6] = getResources().getString(R.string.quest_7_d);
+        }else if (view.getId() == R.id.checkbox_quest_7_d){
+            score += 0.25;
 
         }
-
-        displayNextQuiz();
 
     }
 
 
     public void quest8Answer(View view)
     {
-        if (view.getId() == R.id.quest8_answer_a_container){
-            responses[7] = getResources().getString(R.string.quest_8_a);
+        responseGiven = true;
+        if (view.getId() == R.id.checkbox_quest_8_a){
+            score += 0.25;
 
-        } else if (view.getId() == R.id.quest8_answer_b_container){
-            responses[7] = getResources().getString(R.string.quest_8_b);
+        } else if (view.getId() == R.id.checkbox_quest_8_b){
+            score += 0.25;
 
-        }else if (view.getId() == R.id.quest8_answer_c_container){
-            responses[7] = getResources().getString(R.string.quest_8_c);
+        }else if (view.getId() == R.id.checkbox_quest_8_c){
+            score += 0.25;
 
-        }else if (view.getId() == R.id.quest8_answer_d_container){
-            responses[7] = getResources().getString(R.string.quest_8_d);
+        }else if (view.getId() == R.id.checkbox_quest_8_d){
+            score += 0.25;
 
         }
-        displayNextQuiz();
 
     }
 
 
     public void quest9Answer(View view)
     {
-        if (view.getId() == R.id.quest9_answer_a_container){
-            responses[8] = getResources().getString(R.string.quest_9_a);
+        responseGiven = true;
+        if (view.getId() == R.id.checkbox_quest_9_a){
+            score += 0.25;
 
-        } else if (view.getId() == R.id.quest9_answer_b_container){
-            responses[8] = getResources().getString(R.string.quest_9_b);
+        } else if (view.getId() == R.id.checkbox_quest_9_b){
+            score += 0.25;
 
-        }else if (view.getId() == R.id.quest9_answer_c_container){
-            responses[8] = getResources().getString(R.string.quest_9_c);
+        }else if (view.getId() == R.id.checkbox_quest_9_c){
+            score += 0.25;
 
-        }else if (view.getId() == R.id.quest9_answer_d_container){
-            responses[8] = getResources().getString(R.string.quest_9_d);
+        }else if (view.getId() == R.id.checkbox_quest_9_d){
+            score += 0.25;
 
         }
-        displayNextQuiz();
     }
 
 
     public void quest10Answer(View view)
     {
-        if (view.getId() == R.id.quest10_answer_a_container){
-            responses[9] = getResources().getString(R.string.quest_10_a);
+        responseGiven = true;
+        if (view.getId() == R.id.checkbox_quest_10_a){
+            score += 0.25;
 
-        } else if (view.getId() == R.id.quest10_answer_b_container){
-            responses[9] = getResources().getString(R.string.quest_10_b);
+        } else if (view.getId() == R.id.checkbox_quest_10_b){
+            score += 0.25;
 
-        }else if (view.getId() == R.id.quest10_answer_c_container){
-            responses[9] = getResources().getString(R.string.quest_10_c);
+        }else if (view.getId() == R.id.checkbox_quest_10_c){
+            score += 0.25;
 
-        }else if (view.getId() == R.id.quest10_answer_d_container){
-            responses[9] = getResources().getString(R.string.quest_10_d);
+        }else if (view.getId() == R.id.checkbox_quest_10_d){
+            score += 0.25;
 
         }
-        displayNextQuiz();
 
     }
 
 
     public void quest11Answer(View view)
     {
-        if (view.getId() == R.id.quest11_answer_a_container){
-            responses[10] = getResources().getString(R.string.quest_11_a);
+        responseGiven = true;
+        if (view.getId() == R.id.checkbox_quest_11_a){
+            score += 0.25;
 
-        } else if (view.getId() == R.id.quest11_answer_b_container){
-            responses[10] = getResources().getString(R.string.quest_11_b);
+        } else if (view.getId() == R.id.checkbox_quest_11_b){
+            score += 0.25;
 
-        }else if (view.getId() == R.id.quest11_answer_c_container){
-            responses[10] = getResources().getString(R.string.quest_11_c);
+        }else if (view.getId() == R.id.checkbox_quest_11_c){
+            score += 0.25;
 
-        }else if (view.getId() == R.id.quest11_answer_d_container){
-            responses[10] = getResources().getString(R.string.quest_11_d);
+        }else if (view.getId() == R.id.checkbox_quest_11_d){
+            score += 0.25;
 
         }
-        displayNextQuiz();
 
     }
 
 
     public void quest12Answer(View view)
     {
-        if (view.getId() == R.id.quest12_answer_a_container){
-            responses[11] = getResources().getString(R.string.quest_12_a);
+        responseGiven = true;
+        if (view.getId() == R.id.checkbox_quest_12_a){
+            score += 0.25;
 
-        } else if (view.getId() == R.id.quest12_answer_b_container){
-            responses[11] = getResources().getString(R.string.quest_12_b);
+        } else if (view.getId() == R.id.checkbox_quest_12_b){
+            score += 0.25;
 
-        }else if (view.getId() == R.id.quest12_answer_c_container){
-            responses[11] = getResources().getString(R.string.quest_12_c);
+        }else if (view.getId() == R.id.checkbox_quest_12_c){
+            score += 0.25;
 
-        }else if (view.getId() == R.id.quest12_answer_d_container){
-            responses[11] = getResources().getString(R.string.quest_12_d);
+        }else if (view.getId() == R.id.checkbox_quest_12_d){
+            score += 0.25;
 
-        }else if (view.getId() == R.id.quest12_answer_e_container){
-            responses[11] = getResources().getString(R.string.quest_12_e);
+        }else if (view.getId() == R.id.checkbox_quest_12_e){
+            score += 0.25;
 
         }
-        displayNextQuiz();
 
     }
 
 
     public void quest13Answer(View view)
     {
-        if (view.getId() == R.id.quest13_answer_a_container){
-            responses[12] = getResources().getString(R.string.quest_13_a);
+        responseGiven = true;
+        if (view.getId() == R.id.checkbox_quest_13_a){
+            score += 0.25;
 
-        } else if (view.getId() == R.id.quest13_answer_b_container){
-            responses[12] = getResources().getString(R.string.quest_13_b);
+        } else if (view.getId() == R.id.checkbox_quest_13_b){
+            score += 0.25;
 
-        }else if (view.getId() == R.id.quest13_answer_c_container){
-            responses[12] = getResources().getString(R.string.quest_13_b);
+        }else if (view.getId() == R.id.checkbox_quest_13_c){
+            score += 0.25;
 
-        }else if (view.getId() == R.id.quest13_answer_d_container){
-            responses[12] = getResources().getString(R.string.quest_13_d);
+        }else if (view.getId() == R.id.checkbox_quest_13_d){
+            score += 0.25;
 
         }
 
-        displayFinalThanksContainer();
     }
 
+    public void quest14Answer(View view)
+    {
+        responseGiven = true;
+        if (view.getId() == R.id.checkbox_quest_14_a){
+            score += 0.25;
 
+        } else if (view.getId() == R.id.checkbox_quest_14_b){
+            score += 0.25;
 
+        }else if (view.getId() == R.id.checkbox_quest_14_c){
+            score += 0.25;
 
+        }else if (view.getId() == R.id.checkbox_quest_14_d){
+            score += 0.25;
 
+        }
+    }
+
+    public void submitQuiz(View view)
+    {
+        EditText nbOfBrotherEdit = findViewById(R.id.quest_15_a);
+        if (nbOfBrotherEdit.getText().toString().isEmpty()){
+            Toast.makeText(this, R.string.missingbrothermsg, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        int nbOfBrother = Integer.parseInt(nbOfBrotherEdit.getText().toString());
+        if (nbOfBrother < 4){
+            score += 1;
+        }
+
+        String resultMsg = "Your score is " + score + "\n";
+        if (score < 7){
+            resultMsg += getString(R.string.risque_child_msg);
+
+        } else  if (score < 10){
+            resultMsg += getString(R.string.average_child_msg);
+        } else {
+            resultMsg += getString(R.string.promise_child_msg);
+        }
+
+        Toast.makeText(this, resultMsg, Toast.LENGTH_SHORT).show();
+
+        displayFinalThanksContainer();
+    }
 }
